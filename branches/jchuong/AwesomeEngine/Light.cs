ef48bba6accfdbaca58261dcdfdfb4d64cf4614d
jonchuong <jonchuong@9b7b2ad4-2189-11de-8f7e-2b794b28c5cd>
2009-04-22 22:06:41 +0000

    Still working out some bugs with shadow mapping. Seems like the shader doesn't calculate depth properly.

---
+++ branches/jchuong/AwesomeEngine/Light.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+
+namespace AwesomeEngine
+{
+    public class Light
+    {
+        Vector3 pos;
+        Vector3 lightTarget;
+        
+        public Light(Vector3 pos, Vector3 lightTarget)
+        {
+            this.pos = pos;
+            this.lightTarget = lightTarget;
+        }
+
+        public Vector3 Position
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+
+        public Vector3 LightTarget
+        {
+            get { return lightTarget; }
+            set { lightTarget = value; }
+        }
+
+        public Matrix ViewMatrix
+        {
+            get { return Matrix.CreateLookAt(pos, lightTarget, Vector3.Up); }
+        }
+    }
+}
