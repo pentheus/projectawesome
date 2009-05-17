5a7f1d6ef42cc9ce05f903f8e9bd5af100c14f5d
Scrabbleship <Scrabbleship@9b7b2ad4-2189-11de-8f7e-2b794b28c5cd>
2009-05-17 05:51:02 +0000

    Fixing some files in error.

--- trunk/AwesomeEngine/Items/Item.cs
+++ trunk/AwesomeEngine/Items/Item.cs
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,35 +28,3 @@ namespace AwesomeEngine.Items
         }
     }
 }
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna;
-using Microsoft.Xna.Framework;
-
-namespace AwesomeEngine.Items
-{
-    class Item:GameComponent
-    {
-        ModelInfo model;
-
-        public Item(Game game, ModelInfo model):base(game)
-        {
-            this.model = model;
-        }
-
-        public ModelInfo Model
-        {
-            get { return model; }
-            set { model = value; }
-        }
-
-        public void runScript()
-        {
-
-        }
-    }
-}
->>>>>>> .r105
