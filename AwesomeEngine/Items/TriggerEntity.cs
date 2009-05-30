fd2d6ffb0c7473eef31fc0ad4f105acd5cf49412
Scrabbleship <Scrabbleship@9b7b2ad4-2189-11de-8f7e-2b794b28c5cd>
2009-05-30 23:09:59 +0000

    Adding forgotten Entities classes.

---
+++ AwesomeEngine/Items/TriggerEntity.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AwesomeEngine
+{
+    public class TriggerEntity : LogicEntity
+    {
+        public TriggerEntity(Game game, Model model, Vector3 position, Vector3 scale): 
+            base(game, model,position,scale)
+        {
+            
+        }
+    }
+}
