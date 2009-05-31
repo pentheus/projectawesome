7e97e1388df68183d302d1c9a93cfcae16583c69
Housenuker <Housenuker@9b7b2ad4-2189-11de-8f7e-2b794b28c5cd>
2009-05-31 00:04:25 +0000

    Added functions to Player to load models

---
+++ trunk/AfterDarkGame/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using AwesomeEngine;
+using AwesomeEngine.Items;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AwesomeEngine
+{
+    class Player:DrawableGameComponent
+    {
+        AfterDarkGame.AfterDarkGame game;
+        ModelInfo model;
+        BoundingSphere boundary;
+        int health;
+        List<Item> inventory;
+        FlashLightItem flashlight;
+        Item currentitem;
+
+        public Player(Game game):
+            base(game)
+        {
+            this.game = (AfterDarkGame.AfterDarkGame)game;
+            LoadContent();
+            health = 3;
+            inventory = new List<Item>();
+            flashlight = null;
+        }
+
+        public Player(Game game, List<Item> inv, FlashLightItem light):
+            base(game)
+        {
+            this.game = (AfterDarkGame.AfterDarkGame)game;
+            LoadContent();
+            health = 3;
+            inventory = inv;
+            flashlight = light;
+        }
+
+        public override void LoadContent()
+        {
+            Model playermodel = new Model();
+            ModelInfo.LoadModel(ref playermodel, game.GetScene().Textures, game.GetContent(), game.GetGraphics(), "Player", game.GetScene().Effect);
+        }
+    }
+}
